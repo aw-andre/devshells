6ef49975501d9edfeddd72c994cde6e84965e4eb
aw-andre <aw.andregerard@gmail.com>
2025-03-20 14:52:24 -0400

    feat(embedded-rust): install custom toolchain

--- embedded-rust/flake.nix
+++ embedded-rust/flake.nix
@@ -1,25 +1,31 @@
 {
-  description = "Rust Dev Shell";
+  description = "Embedded Rust Dev Shell";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { nixpkgs, ... }:
-    let pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in {
-      devShells."x86_64-linux".default = pkgs.mkShell rec {
-        packages = with pkgs; [
-          gcc
-          gdb
-          rustc
-          clippy
-          cargo
-          cargo-binutils
-          itm-tools
-          minicom
-        ];
-        shellHook = ''
-          export RUST_BACKTRACE=1
-        '';
-      };
-    };
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            gdb
+            cargo-binutils
+            itm-tools
+            minicom
+            toolchain
+          ];
+          shellHook = ''
+            export RUST_BACKTRACE=1
+          '';
+        };
+      });
 }
