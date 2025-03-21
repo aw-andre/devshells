44f1e1bc708f1fb8f5dfe7557d914d372d94403f
aw-andre <aw.andregerard@gmail.com>
2025-03-20 21:14:13 -0400

    feat(bevy): add full Cargo configuration

--- bevy/flake.nix
+++ bevy/flake.nix
@@ -1,29 +1,37 @@
 {
-  description = "C Dev Shell";
+  description = "Bevy Dev Shell";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { nixpkgs, ... }:
-    let pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in {
-      devShells."x86_64-linux".default = pkgs.mkShell rec {
-        nativeBuildInputs = with pkgs; [ pkg-config ];
-        buildInputs = with pkgs; [
-          udev
-          alsa-lib
-          vulkan-loader
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr # To use the x11 feature
-          libxkbcommon
-          wayland # To use the wayland feature
-        ];
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-        packages = with pkgs; [ gcc rustc clippy cargo ];
-        shellHook = ''
-          export RUST_BACKTRACE=1
-        '';
-      };
-    };
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in {
+        devShells.default = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+            udev
+            alsa-lib
+            vulkan-loader
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+            libxkbcommon
+            wayland
+          ];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+
+          packages = with pkgs; [ pkg-config lld clang toolchain ];
+          shellHook = ''
+            export RUST_BACKTRACE=1
+          '';
+        };
+      });
 }
