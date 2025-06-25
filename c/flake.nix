4fb5d89e7ed9de9e0c220159b28808cbbc022939
aw-andre <aw.andregerard@gmail.com>
2025-06-24 22:00:02 -0400

    use lldb instead of gdb

--- c/flake.nix
+++ c/flake.nix
@@ -3,21 +3,10 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  outputs =
-    {
-      nixpkgs,
-      ...
-    }:
-    let
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in
-    {
-      devShells."x86_64-linux".default = pkgs.mkShell {
-        packages = with pkgs; [
-          gcc
-          gdb
-          valgrind
-        ];
-      };
+  outputs = { nixpkgs, ... }:
+    let pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in {
+      devShells."x86_64-linux".default =
+        pkgs.mkShell { packages = with pkgs; [ gcc lldb valgrind ]; };
     };
 }
